9478cef66626ca6cc4b3413abcbe93c7f19180b5
Carl Walker <carl@bekwam.com>
2023-02-18 13:59:50 -0500

    Updated for latest Keycloak
    Added some fields to RAM-based user store

--- README.md
+++ README.md
@@ -9,8 +9,6 @@ You'll see the new provider in the Server Info and the User Federation pages.
 
 To trace through the code with logging using the following command.
 
-    $ bin/kc start-dev --features-disabled=admin2 --log-level=org.bekwam:TRACE
+    $ bin/kc start-dev --log-level=org.bekwam:TRACE
     
-This disables the admin2 feature in 19 which has a bug as of 2022/10/09 preventing the display of a text field for "My Parameter".
-
 If you're creating a new User Storage SPI for the latest Keycloak, be sure to read this link about the new storage SPI "Map Storage SPI": [changes](https://www.keycloak.org/docs/latest/upgrading/#changes-affecting-developers).
--- src/main/java/org/bekwam/keycloak/storage/spi/helloworld/MemoryUserStorageProvider.java
+++ src/main/java/org/bekwam/keycloak/storage/spi/helloworld/MemoryUserStorageProvider.java
@@ -17,9 +17,12 @@ import org.keycloak.storage.adapter.AbstractUserAdapterFederatedStorage;
 import org.keycloak.storage.user.UserLookupProvider;
 import org.keycloak.storage.user.UserQueryProvider;
 
-import java.util.Collections;
+import java.sql.Timestamp;
+import java.time.ZonedDateTime;
+import java.time.temporal.TemporalField;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -45,14 +48,82 @@ public class MemoryUserStorageProvider implements
     protected final ComponentModel componentModel;
     protected final String myParameter;
 
+    record CustomUser(
+            String username,
+            String password,
+            String email,
+            String firstName,
+            String lastName,
+            ZonedDateTime createdDate
+    ) {}
+
     /**
      * List of users that will appear in the Users screen
      */
-    private final Map<String, String> ramUsers = Map.of(
-            "user1", "password1",
-            "user2", "password2",
-            "user3", "password3"
+    private final List<CustomUser> ramUsers = List.of(
+            new CustomUser("user1", "password1", "user1@example.com", "George", "Washington", ZonedDateTime.parse("2023-01-01T12:00:00.000+05:00")),
+            new CustomUser("user2", "password2", "user2@example.com", "Abraham", "Lincoln", ZonedDateTime.parse("2023-01-15T13:00:00.000+05:00")),
+            new CustomUser("user3", "password3", "user3@example.com", "Franklin", "Roosevelt", ZonedDateTime.parse("2023-02-02T09:00:00.000+05:00"))
     );
+
+
+    public UserModel userModel(RealmModel realmModel, CustomUser user) {
+        return
+                new AbstractUserAdapterFederatedStorage(
+                        MemoryUserStorageProvider.this.keycloakSession,
+                        realmModel,
+                        MemoryUserStorageProvider.this.componentModel) {
+                    @Override
+                    public String getUsername() {
+                        return user.username();
+                    }
+
+                    @Override
+                    public void setUsername(String s) {
+                        throw new UnsupportedOperationException("memory read-only for now");
+                    }
+
+                    @Override
+                    public String getEmail() {
+                        return user.email();
+                    }
+
+                    @Override
+                    public void setEmail(String email) {
+                        throw new UnsupportedOperationException("memory read-only for now");
+                    }
+
+                    @Override
+                    public String getFirstName() {
+                        return user.firstName();
+                    }
+
+                    @Override
+                    public void setFirstName(String firstName) {
+                        throw new UnsupportedOperationException("memory read-only for now");
+                    }
+
+                    @Override
+                    public String getLastName() {
+                        return user.lastName();
+                    }
+
+                    @Override
+                    public void setLastName(String lastName) {
+                        throw new UnsupportedOperationException("memory read-only for now");
+                    }
+
+                    @Override
+                    public Long getCreatedTimestamp() {
+                        return Timestamp.valueOf(user.createdDate().toLocalDateTime()).getTime();
+                    }
+
+                    @Override
+                    public void setCreatedTimestamp(Long timestamp) {
+                        throw new UnsupportedOperationException("memory read-only for now");
+                    }
+                };
+    }
 
     public MemoryUserStorageProvider(
             KeycloakSession keycloakSession,
@@ -63,27 +134,18 @@ public class MemoryUserStorageProvider implements
         this.myParameter = myParameter;
     }
 
-    @Override
     public boolean updateCredential(RealmModel realmModel, UserModel userModel, CredentialInput credentialInput) {
         if (credentialInput.getType().equals(PasswordCredentialModel.TYPE))
             throw new ReadOnlyException("user is read only for this update");
         return false;
     }
 
-    @Override
     public void disableCredentialType(RealmModel realmModel, UserModel userModel, String s) {}
 
-    @Override
-    public Set<String> getDisableableCredentialTypes(RealmModel realmModel, UserModel userModel) {
-        return Collections.EMPTY_SET;
-    }
-
-    @Override
     public boolean supportsCredentialType(String credentialType) {
         return credentialType.equals(PasswordCredentialModel.TYPE);
     }
 
-    @Override
     public boolean isConfiguredFor(RealmModel realmModel, UserModel userModel, String credentialType) {
         return credentialType.equals(PasswordCredentialModel.TYPE);
     }
@@ -97,129 +159,107 @@ public class MemoryUserStorageProvider implements
      * @param credentialInput
      * @return
      */
-    @Override
     public boolean isValid(RealmModel realmModel, UserModel userModel, CredentialInput credentialInput) {
         logger.trace("MemoryUserStorageProvider.isValid()");
-        if( credentialInput.getType().equals("password")) {
-            String password = ramUsers.get(userModel.getUsername());
-            if( password != null ) {
-                return password.equals(credentialInput.getChallengeResponse());
+        if( credentialInput.getType().equals(PasswordCredentialModel.TYPE)) {
+            Optional<CustomUser> user = findUserInRAMStore(userModel.getUsername());
+            if( user.isPresent() ) {
+                logger.debug("found user=" + user.get().username() + "; checking password");
+                return user.get().password().equals(credentialInput.getChallengeResponse());
             }
+            logger.debug("did not find user=" + user.get().username());
             return false;
         } else {
-            throw new UnsupportedOperationException("only credential type 'password' is supported");
+            throw new UnsupportedOperationException("only credential type '" + PasswordCredentialModel.TYPE +"' is supported");
         }
     }
 
-    @Override
     public void close() {}
 
-    @Override
-    public UserModel getUserById(String id, RealmModel realmModel) {
-        logger.trace("getUserById(), id=" + id);
-        StorageId storageId = new StorageId(id);
-        String username = storageId.getExternalId();
-        return getUserByUsername(username, realmModel);
-    }
-
-    @Override
-    public UserModel getUserByUsername(String username, RealmModel realmModel) {
-        if( ramUsers.containsKey(username) ) {
-            return new AbstractUserAdapterFederatedStorage(keycloakSession, realmModel, componentModel) {
-                @Override
-                public String getUsername() {
-                    return username;
-                }
-
-                @Override
-                public void setUsername(String s) {
-                    throw new UnsupportedOperationException("memory read-only for now");
-                }
-            };
-        }
-        return null;
-    }
-
-    @Override
-    public UserModel getUserByEmail(String s, RealmModel realmModel) {
-        return null;
-    }
-
-    @Override
     public int getUsersCount(RealmModel realm) {
-        return this.getUsers(realm).size();
+        return (int)this.getUsersStream(realm).count();
     }
 
-    @Override
+
     public Stream<UserModel> getUsersStream(RealmModel realm) {
         return this.getUsersStream(realm, 0, Integer.MAX_VALUE);
     }
 
-    @Override
     public Stream<UserModel> getUsersStream(RealmModel realmModel, Integer first, Integer max) {
         return ramUsers
-                .entrySet()
                 .stream()
                 .skip(first)
                 .limit(max - first+1)
-                .map( kv -> new AbstractUserAdapterFederatedStorage(keycloakSession, realmModel, componentModel) {
-                    @Override
-                    public String getUsername() {
-                        return kv.getKey();
-                    }
-
-                    @Override
-                    public void setUsername(String s) {
-                        throw new UnsupportedOperationException("memory read-only for now");
-                    }
-                });
+                .map( u -> userModel(realmModel, u));
     }
 
-    @Override
     public Stream<UserModel> searchForUserStream(RealmModel realm, String search) {
         logger.trace("searchForUserStream() #1");
         return UserQueryProvider.Streams.super.searchForUserStream(realm, search);
     }
 
-    @Override
-    public Stream<UserModel> searchForUserStream(RealmModel realm, String search, Integer firstResult, Integer maxResults) {
+    public Stream<UserModel> searchForUserStream(RealmModel realmModel, String search, Integer firstResult, Integer maxResults) {
         logger.trace("searchForUserStream() #2; search=" + search);
         return ramUsers
-                .entrySet()
                 .stream()
-                .map( kv -> new AbstractUserAdapterFederatedStorage(keycloakSession, realm, componentModel) {
-                    @Override
-                    public String getUsername() {
-                        return kv.getKey();
-                    }
-
-                    @Override
-                    public void setUsername(String s) {
-                        throw new UnsupportedOperationException("memory read-only for now");
-                    }
-                });
+                .map( u -> userModel(realmModel, u));
     }
 
-    @Override
     public Stream<UserModel> searchForUserStream(RealmModel realm, Map<String, String> params) {
         logger.trace("searchForUserStream() #3");
         // this method needs to exist for paging
         return this.searchForUserStream(realm, "", 0, Integer.MAX_VALUE);
     }
 
-    @Override
     public Stream<UserModel> searchForUserStream(RealmModel realmModel, Map<String, String> params, Integer firstResult, Integer maxResults) {
         logger.trace("searchForUserStream() #4; params=" + params);
         return this.searchForUserStream(realmModel, "", firstResult, maxResults);
     }
 
-    @Override
     public Stream<UserModel> getGroupMembersStream(RealmModel realmModel, GroupModel groupModel, Integer firstResult, Integer maxResults) {
         return null;
     }
 
-    @Override
     public Stream<UserModel> searchForUserByUserAttributeStream(RealmModel realmModel, String s, String s1) {
         return null;
+    }
+
+    public UserModel getUserByEmail(RealmModel realmModel, String s) {
+        return null;
+    }
+
+    @Override
+    public UserModel getUserByUsername(RealmModel realmModel, String username) {
+        var user = findUserInRAMStore(username);
+        if( user.isPresent() ) {
+            return userModel(realmModel, user.get());
+        }
+        return null;
+    }
+
+    @Override
+    public UserModel getUserById(RealmModel realmModel, String id) {
+        logger.trace("getUserById(), id=" + id);
+        StorageId storageId = new StorageId(id);
+        String username = storageId.getExternalId();
+        return getUserByUsername(realmModel, username);
+    }
+
+    @Override
+    public Stream<String> getDisableableCredentialTypesStream(RealmModel realmModel, UserModel userModel) {
+        return Stream.of(PasswordCredentialModel.TYPE);
+    }
+
+    private Optional<CustomUser> findUserInRAMStore(String username) {
+        if( username == null || username.isBlank() ) {
+            return Optional.empty();
+        }
+        return ramUsers
+                .stream()
+                .filter(
+                        u -> u.username().equals(username)
+                )
+                .findFirst();
+
     }
 }
